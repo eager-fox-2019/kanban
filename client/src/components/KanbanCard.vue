54ab436643228a83f768129cf69461223eb0e1d2
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-06-29 11:11:39 +0700

    kanban card

--- client/src/components/KanbanCard.vue
+++ client/src/components/KanbanCard.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="margin-l background-white">
-        <p>{{kanbanText}}</p>
+    <div class="margin-m padding-s background-white flex flex-row justify-content-space-between">
+      <div>
+        <strong><p class="lead">{{kanbanDetail.description}}</p></strong>
+        <p>{{kanbanDetail.createdAt}}</p>
+      </div>
+      <button class="button button-error button-border">
+        <i class="fa fa-trash" aria-hidden="true"></i>
+      </button>
     </div>
 </template>
 
 <script>
 export default {
-  props: ['kanbanText']
+  props: ['kanbanDetail']
 }
 </script>
