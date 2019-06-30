abbbfde8382308779355e3bf585b30167cbe2b63
muhamadnabila <mbilalghz.gmail.com>
2019-07-01 00:07:33 +0700

    kanban

---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+    <div lass="card border-primary mb-3" style="max-width: 18rem;">
+      <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div v-bind:class= "'card-header ' + color">{{card}}</div>
+              <div class="card-body text-primary">
+                <div v-for="task in tasks" :key="task.id">
+                    <Task v-bind:task="task" v-bind:card="card" />
+                </div>
+              </div>
+          </div>
+        </div>
+        
+        
+</template>
+
+<script>
+import Task from './Task'
+export default {
+    data () {
+      return {
+        color : 'bg-danger'
+      }
+    },
+    props : ['card','tasks'],
+    components : {
+        Task
+    },
+    mounted () {
+        console.log(this.card)
+        if (this.card == 'Back-Log') {
+          this.color = 'bg-danger'
+        }
+        else if (this.card == 'To-Do') {
+          this.color = 'bg-primary'
+        }
+        else if (this.card == 'Doing') {
+          this.color = 'bg-warning'
+        }
+        else if (this.card == 'Done') {
+          this.color = 'bg-success'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: "kamvan-6f700",
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  export default db
